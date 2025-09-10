2473caeed011e753286ce31f4a85633207fa9c0c
Vaishnavi Mittal <vishumitl24@gmail.com>
2025-09-11 01:08:21 +0530

    Time: 3 ms (43.11%) | Memory: 45.9 MB (31.97%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+
+        int n = nums.length;
+        if(n==1)return true;
+        int idx = n-1;
+        int jumps=nums[0];
+        int i=0;
+        while(i<n){
+            if(jumps-(idx-i) >=0) return true;
+            else{
+                int temp=jumps;
+                int flag=0;
+                while(temp>0){
+                    temp--;
+                    i++;
+                    if(nums[i]>=jumps){
+                        jumps=nums[i];
+                        flag=1;
+                        break;
+                    }else{
+                        if(nums[i]-(n-1-i)>=0){
+                            flag=2;
+                            break;
+                        }
+                    }
+                }
+                if(flag==0)jumps=nums[i];
+                if(flag==2) break;
+                if(jumps==0)break;
+            }
+        }
+        if(jumps==0)return false;
+        //else if(flag==2) return true;
+        else return true;
+
+    }
+}
