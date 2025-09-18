d5aed6609c8210bdd2a48f31ce67ec456327ecd6
Vaishnavi Mittal <vishumitl24@gmail.com>
2025-09-18 12:48:40 +0530

    Time: 6 ms (85.63%) | Memory: 43 MB (78.77%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,27 +1,41 @@
 class Solution {
     public String reverseWords(String s) {
-        s=s+' ';
-        ArrayList<String> list = new ArrayList<>();
-        int n=s.length();
-        int i=0;
-        while(i<n && s.charAt(i)==' ')i++;
-        String word="";
-        while(i<n){
-            if(s.charAt(i)==' '){
-                System.out.println(word);
-                if(word.length()>0)list.add(word);
-                word="";
-            }else word+=s.charAt(i);
-            i++;
+        s = s.trim();
+        String[] words  = s.split("\\s+");
+        StringBuilder sb = new StringBuilder();
+        for(int i=words.length-1;i>=0;i--){
+            sb.append(words[i]);
+            if(i>0)sb.append(' ');
         }
-        String ans="";
-        int len = list.size();
-        System.out.print(len);
-        for(int j=len-1;j>=0;j--){
-            ans+=list.get(j);
-            if(j!=0)ans+=' ';
-        }
-        return ans;
+        return sb.toString();
 
     }
 }
+
+// class Solution {
+//     public String reverseWords(String s) {
+//         s=s+' ';
+//         ArrayList<String> list = new ArrayList<>();
+//         int n=s.length();
+//         int i=0;
+//         while(i<n && s.charAt(i)==' ')i++;
+//         String word="";
+//         while(i<n){
+//             if(s.charAt(i)==' '){
+//                 System.out.println(word);
+//                 if(word.length()>0)list.add(word);
+//                 word="";
+//             }else word+=s.charAt(i);
+//             i++;
+//         }
+//         String ans="";
+//         int len = list.size();
+//         System.out.print(len);
+//         for(int j=len-1;j>=0;j--){
+//             ans+=list.get(j);
+//             if(j!=0)ans+=' ';
+//         }
+//         return ans;
+
+//     }
+// }
