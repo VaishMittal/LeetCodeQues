37eb8461bee5b695c4dfe4f0442f82683279bb52
Vaishnavi Mittal <vishumitl24@gmail.com>
2025-09-24 12:55:28 +0530

    Time: 2 ms (24.63%) | Memory: 41.8 MB (74.48%) - LeetSync

---
+++ 68-text-justification/text-justification.java
@@ -0,0 +1,56 @@
+class Solution {
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        int i = 0;
+        int n = words.length;
+        List<String> list = new ArrayList<>();
+        int left, word, last;
+        while (i < n) {
+            int curlen = 0;
+            String ans = "";
+            word = 1;
+            last = 0;
+            ans += words[i];
+            curlen += words[i].length();
+            i++;
+            while (i < n && (curlen + words[i].length() + 1) <= maxWidth) {
+                ans += " ";
+                ans += words[i];
+                curlen += words[i].length() + 1;
+                word++;
+                i++;
+            }
+            if (i == n) last = 1;
+            left = maxWidth - curlen;
+            ans = addSpace(ans, left, word, last);
+            list.add(ans);
+        }
+        return list;
+    }
+
+    public static String addSpace(String ans, int left, int word, int last) {
+        if (last == 1 || word == 1) {
+            // Left-justify: just add spaces to the end
+            while (left > 0) {
+                ans += " ";
+                left--;
+            }
+        } else {
+            // Full-justify: distribute spaces evenly
+            String[] arr = ans.split(" ");
+            int gaps = word - 1;
+            int totalSpaces = left + gaps; // spaces already present + left to add
+            int minSpace = totalSpaces / gaps;
+            int extra = totalSpaces % gaps;
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < arr.length; j++) {
+                sb.append(arr[j]);
+                if (j < gaps) {
+                    for (int s = 0; s < minSpace; s++) sb.append(" ");
+                    if (j < extra) sb.append(" ");
+                }
+            }
+            ans = sb.toString();
+        }
+        return ans;
+    }
+}
