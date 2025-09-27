0f035fe983050266e058d1e9b6cf2a3bb9b11a82
Vaishnavi Mittal <vishumitl24@gmail.com>
2025-09-27 16:22:37 +0530

    Time: 5 ms (77.15%) | Memory: 57.6 MB (78.66%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int maxArea(int[] arr) {
+        int n= arr.length;
+        if(n==1 || n==0)return 0;
+        if(n==2){
+            return Math.min(arr[0],arr[1])*Math.min(arr[0],arr[1]);
+        }
+        int start=0;
+        int end = n-1;
+        int ans=-1;
+        while(start<end){
+            int mini=Math.min(arr[start],arr[end]);
+            ans=Math.max(ans,mini*(end-start));
+            if(arr[start]>=arr[end]){
+                end--;
+            }else{
+                start++;
+            }
+        }
+        return ans;
+    }
+}
+
+// class Solution {
+//     public int maxArea(int[] arr) {
+//         int n= arr.length;
+//         if(n==1 || n==0)return 0;
+//         if(n==2){
+//             return Math.min(arr[0],arr[1])*Math.min(arr[0],arr[1]);
+//         }
+//         int ans=-1;
+//         for(int i=0;i<n;i++){
+//             for(int j=i+1;j<n;j++){
+//                 int mini=Math.min(arr[i],arr[j]);
+//                 ans=Math.max(ans,mini*(j-i));
+//             }
+//         }
+//         return ans;
+//     }
+// }
