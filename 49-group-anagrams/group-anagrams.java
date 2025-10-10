a1f0ca5cabfdbd8e089dbaf90d39bb5223a76550
Vaishnavi Mittal <vishumitl24@gmail.com>
2025-10-10 22:37:08 +0530

    Time: 6 ms (98.67%) | Memory: 48.6 MB (17.60%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,38 +1,60 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] arr) {
-        List<List<String>> ans = new ArrayList<>();
-        int n=arr.length;
-        if(n==0 )return ans;
-        
-        // if (n==1){
-        //     ans.add(arr[0]);
-        //     return ans;
-        // } 
-
-        Map<String ,Integer> m1=new HashMap<>();
-        Map<String ,Integer> m2=new HashMap<>();
-        int c=1;
-        for(int i=0;i<n;i++){
-            char[] arr2=arr[i].toCharArray();
-            Arrays.sort(arr2);
-            String str =new String(arr2);
-            if(!m2.containsKey(str)){
-                m2.put(str,c);
-                c++;
-            }
-            m1.put(arr[i],m2.get(str));
-        }
-        c--;
-        while(c>0){
-            List<String> inner = new ArrayList<>();
-            for(int i=0;i<n;i++){
-                if(m1.get(arr[i])==c){
-                    inner.add(arr[i]);
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> m = new HashMap<>();
+        for (String s : strs) {
+            if (s != null) {
+                String sortedString = sortString(s);
+                if (!m.containsKey(sortedString)) {
+                    m.put(sortedString, new ArrayList<>());
                 }
+                m.get(sortedString).add(s);
             }
-            ans.add(inner);
-            c--;
         }
-        return ans;
+        return new ArrayList<>(m.values());
+    }
+
+    private String sortString(String input) {
+        char[] arr = input.toCharArray();
+        Arrays.sort(arr);
+        return new String(arr);
     }
 }
+
+// class Solution {
+//     public List<List<String>> groupAnagrams(String[] arr) {
+//         List<List<String>> ans = new ArrayList<>();
+//         int n=arr.length;
+//         if(n==0 )return ans;
+        
+//         // if (n==1){
+//         //     ans.add(arr[0]);
+//         //     return ans;
+//         // } 
+
+//         Map<String ,Integer> m1=new HashMap<>();
+//         Map<String ,Integer> m2=new HashMap<>();
+//         int c=1;
+//         for(int i=0;i<n;i++){
+//             char[] arr2=arr[i].toCharArray();
+//             Arrays.sort(arr2);
+//             String str =new String(arr2);
+//             if(!m2.containsKey(str)){
+//                 m2.put(str,c);
+//                 c++;
+//             }
+//             m1.put(arr[i],m2.get(str));
+//         }
+//         c--;
+//         while(c>0){
+//             List<String> inner = new ArrayList<>();
+//             for(int i=0;i<n;i++){
+//                 if(m1.get(arr[i])==c){
+//                     inner.add(arr[i]);
+//                 }
+//             }
+//             ans.add(inner);
+//             c--;
+//         }
+//         return ans;
+//     }
+// }
