37c6f830f5ca8dfe6e1b10579e5fc839f292f822
Vaishnavi Mittal <vishumitl24@gmail.com>
2025-11-08 14:12:22 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (5.39%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -13,22 +13,52 @@
  *     }
  * }
  */
+// class Solution {
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         List<Integer> ans = new ArrayList<>();
+//         if(root==null)return ans;
+//         if(root.left== null && root.right==null) {
+//             ans.add(root.val);
+//             return ans;
+//         }
+//         postOrder(root,ans);
+//         return ans;
+//     }
+//     public void postOrder(TreeNode node,List<Integer> ans){
+//         if(node==null) return;
+
+//         postOrder(node.left,ans);
+//         postOrder(node.right,ans);
+//         ans.add(node.val);
+//     }
+// }
+
+
+
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
+        Stack<TreeNode> st1 = new Stack<>();
+        Stack<TreeNode> st2 = new Stack<>();
+
         if(root==null)return ans;
         if(root.left== null && root.right==null) {
             ans.add(root.val);
             return ans;
         }
-        postOrder(root,ans);
+        //using two stack
+        st1.push(root);
+        while(!st1.isEmpty()){
+            TreeNode top = st1.pop();
+            st2.push(top);
+            if(top.left!=null)st1.push(top.left);
+            if(top.right!=null)st1.push(top.right);
+        }
+        while(!st2.isEmpty()){
+            ans.add(st2.pop().val);
+        }
+
         return ans;
     }
-    public void postOrder(TreeNode node,List<Integer> ans){
-        if(node==null) return;
-
-        postOrder(node.left,ans);
-        postOrder(node.right,ans);
-        ans.add(node.val);
-    }
+   
 }
