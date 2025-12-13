6b3f10d3d9f4d3c3232d2a8ed43d2a2354bc509a
Vaishnavi Mittal <vishumitl24@gmail.com>
2025-12-13 22:51:44 +0530

    Time: 84 ms (21.25%) | Memory: 237.4 MB (5.57%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,29 +1,38 @@
 class Solution {
     public int lengthOfLIS(int[] arr) {
         int n = arr.length;
-        int[][] dp = new int[n][n];
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
+        int[][] dp = new int[n+1][n+1];
+     
+        for(int cur=n-1;cur>=0;cur--){
+            for(int prev=cur-1;prev>=-1;prev--){
+                int notpick = dp[cur+1][prev+1];
+                int pick=0;
+                if(prev==-1 || arr[cur]>arr[prev]){
+                    pick=1+dp[cur+1][cur+1];
+                }
+                dp[cur][prev+1] = Math.max(pick,notpick);
+            }
         }
-        return find(arr,0,-1,dp);
-    }
 
-    public int find(int[] arr, int idx, int prev,int[][] dp){
-        if(idx==arr.length-1){
-            if(prev==-1 || arr[prev]<arr[idx])return 1;
-            else return 0;
-        }
-        if(dp[idx][prev+1]!=-1)return dp[idx][prev+1];
-        int notpick = find(arr,idx+1,prev,dp);//not  pick
-        int pick=0;
-        if(prev==-1 || arr[prev]<arr[idx]){
-            pick = 1+ find(arr,idx+1,idx,dp);
-        } //pick
-
-        return dp[idx][prev+1] =  Math.max(pick,notpick);
-
+        return dp[0][0];
     }
 }
+//     public int find(int[] arr, int idx, int prev,int[][] dp){
+//         if(idx==arr.length-1){
+//             if(prev==-1 || arr[prev]<arr[idx])return 1;
+//             else return 0;
+//         }
+//         if(dp[idx][prev+1]!=-1)return dp[idx][prev+1];
+//         int notpick = find(arr,idx+1,prev,dp);//not  pick
+//         int pick=0;
+//         if(prev==-1 || arr[prev]<arr[idx]){
+//             pick = 1+ find(arr,idx+1,idx,dp);
+//         } //pick
+
+//         return dp[idx][prev+1] =  Math.max(pick,notpick);
+
+//     }
+// }
 
 
 
