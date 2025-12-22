af4626dd3693562145e9af92828c2cdce6d586fd
Vaishnavi Mittal <vishumitl24@gmail.com>
2025-12-22 13:02:29 +0530

    Time: 26 ms (20.75%) | Memory: 48 MB (13.61%) - LeetSync

---
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -0,0 +1,42 @@
+class Pair{
+    int row;
+    int col;
+    int steps;
+    Pair(int steps, int row, int col){
+        this.row=row;
+        this.col=col;
+        this.steps=steps;
+    }
+}
+
+class Solution {
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int n = grid.length;
+        if(grid[0][0]==1)return -1;
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->(a.steps-b.steps));
+        pq.add(new Pair(1,0,0));
+        int minsteps=Integer.MAX_VALUE;
+        while(!pq.isEmpty()){
+            Pair p =pq.poll();
+            int row=p.row;
+            int col=p.col;
+            int steps=p.steps;
+            if(row==n-1 && col==n-1){
+                minsteps=Math.min(minsteps,steps);
+            }
+
+            int[] dr = {-1,-1,-1,1,1,1,0,0};
+            int[] dc = {-1,0,1,-1,0,1,1,-1};
+            for(int i=0;i<8;i++){
+                int nr = row+dr[i];
+                int nc = col+dc[i];
+                if(nr<n && nr>=0 && nc<n && nc>=0 && grid[nr][nc]==0){
+                    pq.add(new Pair(steps+1,nr,nc));
+                    grid[nr][nc]=1;
+                }
+            }
+        }
+        if(minsteps==Integer.MAX_VALUE)return -1;
+        else return minsteps;
+    }
+}
